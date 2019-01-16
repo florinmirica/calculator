bc1f9150ec239c2f67ac0306c50624c896be8aa8
Florin Mirica and Porter Savage <porter@>
2019-01-16 13:12:06 -0800

    Add links to scripts.js, stylesheet.css, bootstrap.css, & jquery.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var num1 = parseInt(prompt("Enter First Number:"));
+var num2 = parseInt(prompt("Enter Second Number:"));
+
+var sum = function(one, two){
+	return num1 + num2;
+}
+
+var result = sum(num1 + num2)
+alert(result);
