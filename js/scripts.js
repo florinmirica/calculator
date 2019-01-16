7082145646c74775b91c84065e05bdd95ce70fcf
Florin Mirica and Porter Savage <porter@>
2019-01-16 13:27:00 -0800

    add more functions for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 var num1 = parseInt(prompt("Enter First Number:"));
 var num2 = parseInt(prompt("Enter Second Number:"));
 
-var sum = function(one, two){
+var add = function(one, two){
 	return num1 + num2;
 }
 
-var result = sum(num1 + num2)
+var subtract = function(one, two){
+	return num1 - num2;
+}
+
+var multiply = function(one, two){
+	return num1 * num2;
+}
+
+var divide = function(one, two){
+	return num1 / num2;
+}
+
+var result = add(num1 + num2)
 alert(result);
